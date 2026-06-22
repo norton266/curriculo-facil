18f44181bb1cd5670f33f251ff0e9bfb2934b861
norton266 <njaguarema@gmail.com>
2026-06-22 14:00:07 -0300

    Pequeno ajuste em foto.py e experiências.html

--- services/componentes/foto.py
+++ services/componentes/foto.py
@@ -1,33 +1,16 @@
-from reportlab.platypus import (
-    Image,
-    Table,
-    TableStyle
-)
+from reportlab.platypus import Image, Spacer, Paragraph
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib import colors
 
 
-def adicionar_foto(
-        conteudo,
-        caminho_foto
-):
+def adicionar_foto(conteudo, caminho_foto):
 
-    foto = Image(
-        caminho_foto,
-        width=100,
-        height=100
-    )
+    if not caminho_foto:
+        return
 
-    tabela = Table(
-        [[foto]]
-    )
+    foto = Image(caminho_foto)
+    foto.drawWidth = 100
+    foto.drawHeight = 100
 
-    tabela.setStyle(
-        TableStyle(
-            [
-                ('ALIGN', (0, 0), (-1, -1), 'CENTER')
-            ]
-        )
-    )
-
-    conteudo.append(
-        tabela
-    )
+    conteudo.append(foto)
+    conteudo.append(Spacer(1, 10))
